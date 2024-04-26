eb1b2fb28842c6c5ebeefaf66e34df82f804ca72
Fredrik Ås <fredaas86@gmail.com>
2024-04-26 10:02:03 +0200

    Fix wrong macro name

--- htable/crc32_generate.c
+++ htable/crc32_generate.c
@@ -4,15 +4,15 @@
 
 int main(void)
 {
-    crc32_generate(TL_POLY_NORMAL);
+    crc32_generate(HTABLE_POLY_NORMAL);
     crc32_dump("./poly_normal.txt");
 
-    crc32_generate(TL_POLY_REVERSED);
+    crc32_generate(HTABLE_POLY_REVERSED);
     crc32_dump("./poly_reversed.txt");
 
-    crc32_generate(TL_POLY_RECIPROCAL);
+    crc32_generate(HTABLE_POLY_RECIPROCAL);
     crc32_dump("./poly_reciprocal.txt");
 
-    crc32_generate(TL_POLY_REVERSED_RECIPROCAL);
+    crc32_generate(HTABLE_POLY_REVERSED_RECIPROCAL);
     crc32_dump("./poly_reversed_reciprocal.txt");
 }
